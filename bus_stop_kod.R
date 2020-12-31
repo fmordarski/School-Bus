e25751b7508e339c9f432fc70a82032874c7162d
fmordarski <fmordarski@gmail.com>
2020-12-31 12:55:53 +0100

    zmodyfikowana funkcja kosztu

--- bus_stop_kod.R
+++ bus_stop_kod.R
@@ -1,6 +1,7 @@
 remove(list = ls())
 ####################### Macierz kosztow #############################
-setwd('C:\\Users\\lenovo\\Desktop\\projekt\ nmo')
+# setwd('C:\\Users\\lenovo\\Desktop\\projekt\ nmo')
+setwd('C:/Users/Uzytkownik/Documents/Studia/nieklasyczne metody/School-Bus')
 koszty <- read.csv('koszty.csv', sep = ';', dec = ",")
 koszty <- koszty[,1:11]
 rownames(koszty) <- koszty[,1]
@@ -29,36 +30,28 @@ b1 = c(1,4,10)
 b2 = c(2,3,5,8, 10)
 b3 = c(6,7,9,10)
 
-
-f = function(b1, b2, b3, D)
+b = list(b1, b2, b3)
+f = function(b, D)
 {
-  n1 = length(b1)
-  d1 = 0
-  for (i in 1 : (n1-1))
-  {
-    d1 = d1 + D[b1[i], b1[i+1]] * Y[b1[i]] 
+  number_buses = length(b)
+  n_list = lapply(b, length)
+  d_list = rep(0, number_buses)
+  y_list = rep(0, number_buses)
+  for (bus in 1:number_buses){
+    for (n in 1 : (n_list[[bus]]-1))
+    { 
+      y_list[bus] = y_list[bus] + Y[b[[bus]][n]]
+      d_list[bus] = d_list[bus] + D[b[[bus]][n], b[[bus]][n+1]] * y_list[bus]
+      if (bus == 1){
+        print(y_list[bus])
+        print(d_list[bus])
+      }
+      }
   }
   
-  n2 = length(b2)
-  d2 = 0
-  for (i in 1 : (n2-1))
-  {
-    d2 = d2 + D[b2[i], b2[i+1]] * Y[b2[i]] 
-  }
-  
-  n3 = length(b3)
-  d3 = 0
-  for (i in 1 : (n3-1))
-  {
-    d3 = d3 + D[b3[i], b3[i+1]] * Y[b3[i]] 
-  }
-  
-  d = d1 + d2 + d3
-  
-  
-  return(d)
+  return(sum(d_list))
 }
 
-f(b1,b2,b3, Tk)
+f(b, Tk)
 
 
