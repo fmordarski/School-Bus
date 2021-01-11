6c5040e663b01fd418fc9451df5cca57a3b0fbe9
kacperk77 <kacperk77>
2021-01-11 14:50:20 +0100

    losowanie pierwszego rozwiazania

--- bus_stop_kod.R
+++ bus_stop_kod.R
@@ -1,7 +1,8 @@
 remove(list = ls())
 ####################### Macierz kosztow #############################
-setwd('C:\\Users\\lenovo\\Desktop\\project nmo')
+setwd('C:\\Users\\lenovo\\Desktop\\projekt nmo stary')
 #setwd('C:/Users/Uzytkownik/Documents/Studia/nieklasyczne metody/School-Bus')
+# setwd("C:\\Users\\pc\\Desktop\\Nieklasyczne metody optymalizacji\\projekt")
 koszty <- read.csv('koszty.csv', sep = ';', dec = ",")
 koszty <- koszty[,1:11]
 rownames(koszty) <- koszty[,1]
@@ -14,44 +15,35 @@ rownames(Tk) <- c(1:10)
 Tk <- as.matrix(Tk)
 
 
-
+set.seed(1234)
 ####################### Zmienne #########################
 B <- 3
 K = 1:B
 # Tk ij - odleglosc podrozy z przystanku i do przystanka j  
 N = nrow(Tk)
 S = 1:N
-C = c(50,75,60)
 
-Y = sample.int(15, 9)
 
+Y = round(rnorm(9,mean=12,sd=3),0)
 #Wylosowana trasa poczatkowa b
-#b = split(1:9, sample(3, 9 , repl = TRUE))
-#
-#for (i in 1:length(b))
-#{
-#  b[[i]] = c(b[[i]], 10)
-#  
-#}
 
 
-
-#Ustalona trasa poczatkowa b
-b1 = c(1,4, 9, 10)
-b2 = c(2,3,5,8, 10)
-b3 = c(6,7,10)
-
-b = list(b1, b2, b3)
+#funkcja do sprawdzenia liczby przystankow 
+czy_n_przystankow <- function(pass){
+  n_przystankow <- c()
+  for(i in 1:length(b)){
+    n_przystankow[i] <- ifelse(length(b[[i]])>2, 1, 0)
+  }
+  return(sum(n_przystankow))}
 
 
 constraint <- function(pass){
   check <- c()
   for(i in 1:3){
-    check[i] <- ifelse(pass[i]<45,1,0)
+    check[i] <- ifelse(pass[i]<50,1,0)
   }
   return(sum(check))}
 
-constraint(wyn)
 
 
 
@@ -94,10 +86,53 @@ f2 = function(b, D)
 
 
 
+
+
+
+repeat 
+{ 
+  #losujemy przystnaki dla kazdego busa
+  b = split(1:9, sample(3, 9 , repl = TRUE))
+  
+  
+  #dodajemy sgh na koncu "10"
+  for (i in 1:length(b))
+  {
+    b[[i]] = c(b[[i]], 10)
+    
+  }
+  
+  if(czy_n_przystankow(b) == 3 &
+     constraint(f2(b, Tk) == 3)) 
+  {
+    break
+  }
+  
+}
+print(b)
+
+
+
+
+
+
+
+#Ustalona trasa poczatkowa b
+#b1 = c(1,4, 9, 10)
+#b2 = c(2,3,5,8, 10)
+#b3 = c(6,7,10)
+
+#b = list(b1, b2, b3)
+
+
+
+
+
+
+
 wyn =f2(b, Tk)
 
-
-
+constraint(wyn)
 
 
 
@@ -133,16 +168,17 @@ SA = function(stops, buses, x, D, f, f2, constraint, delta, t, alpha, maxIt)
           wyn = f2(x_c, D)
           cons = constraint(wyn)
           
+          
+        }
         
-          }
-          
-          
-          
+        
+        
       }
       
       
     }
-    if (cons < 3) next
+    if (exists('cons') == FALSE) next
+    if (cons < 3) next 
     
     # Symulacja przejscia do kandydata na rozwiazanie.
     A = min(1, exp(-(f(x_c, D) - f(x, D)) / t))
@@ -163,14 +199,53 @@ SA = function(stops, buses, x, D, f, f2, constraint, delta, t, alpha, maxIt)
   opt_f.hist = min(unlist(out$f.hist)) #optymalna wartosc funkcji celu (minimalna)
   index.min = which.min(unlist(out$f.hist)) #indeks z optymalna wartoscia funkcji celu
   x.opt = out$x.hist[[index.min]] #x optymalny, ktory wyznacza optymalna wartosc f. celu
+  f.hist = unlist(out$f.hist)
   
-  
-  ret = list(x.opt, opt_f.hist)
+  ret = list(x.opt, opt_f.hist,f.hist)
   
   return(ret)
   
 }
 
-SA(S, B, b, Tk, f, f2, constraint, delta, t, alpha, maxIt)
+Z <- SA(S, B, b, Tk, f, f2, constraint, delta, t, alpha, maxIt)
+plot(1:length(Z[[3]]),Z[[3]])
 
- 
+max_buses = 10
+cost_bus = 20
+outputs = c()
+
+for(bus in 3:max_buses){
+  # od tego rozwi1zania wyznaczamy dla kolejnych busów
+  initial_solution = b
+  if(bus > 3){
+    # ile musimy dodaa przystanków do pojedynczego nowego busa
+    buses_to_change = round((length(S)-1)/bus, 0)
+    for(i in 1:(bus-3)){
+      # przystanki w nowym busie
+      new_bus = c()
+      for(new_stop in 1:buses_to_change){
+        # jak1 d3ugooa przystanków maj1 obecne busy
+        length_buses <- sapply(initial_solution, length)
+        # wyznaczamy przystanki tego busa, który ma ich najwiecej
+        old_bus <- initial_solution[[which.max(length_buses)]]
+        # losujemy przystanek, który zabieramy
+        stop_to_change <- sample(old_bus[-length(old_bus)], 1)
+        # dodajemy ten przystanek do nowego busa
+        new_bus <- append(new_bus, stop_to_change)
+        # usuwamy ten przystanek z poprzedniego busa
+        initial_solution[[which.max(length_buses)]] = old_bus[!old_bus %in% stop_to_change]
+      }
+      # dodajemy SGH - 10 do busa
+      new_bus <- append(new_bus, length(S))
+      # dodajemy tego busa do naszego pocz1tkowego rozwi1zania
+      initial_solution <- c(initial_solution, list(new_bus))
+    }
+    
+  }
+  
+  new_value <- SA(S, bus, initial_solution, Tk, f, f2, constraint, delta, t, alpha, maxIt)[[2]]
+  new_value <- 2*new_value+(bus*cost_bus)
+  outputs <- append(outputs, new_value)
+  
+}
+
